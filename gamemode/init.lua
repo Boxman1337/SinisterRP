6b8ac46d4f9eadf7504fe3fe3f97deb46996c452
Axel <you@example.com>
2022-10-22 01:10:26 +0200

    Basic GUI Structure

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -1,5 +1,3 @@
 AddCSLuaFile("cl_init.lua")
-AddCSLuaFile("shared.lua")
-AddCSLuaFile("modules/hud/cl_hud.lua")
 
-include("shared.lua")
+AddCSLuaFile("modules/hud/cl_hud.lua")
--- gamemode/modules/hud/cl_hud.lua
+++ gamemode/modules/hud/cl_hud.lua
@@ -1,6 +1,56 @@
 
 function DrawHUD()
-    draw.RoundedBox(0, 0, 0, 100, 100, Color(200, 200, 200, 180))
+    
+    local ScreenWidth = ScrW()
+    local ScreenHeight = ScrH()
+
+    local ScreenCenterpointWidth = ScreenWidth / 2
+
+    local BaseBoxWidth = 600
+    local BaseBoxHeight = 90
+    local BaseBoxX = ScreenCenterpointWidth - BaseBoxWidth / 2
+    local BaseBoxY = ScreenHeight - BaseBoxHeight
+
+    local BaseBoxCornerRadius = 25
+
+    local BaseBoxRoundTopLeft = true
+    local BaseBoxRoundTopRight = true
+    local BaseBoxRoundBottomLeft = false
+    local BaseBoxRoundBottomRight = false
+
+    local L1ScaleOffset = 10
+    
+    local BaseBoxL1Width = BaseBoxWidth - L1ScaleOffset
+    local BaseBoxL1Height = BaseBoxHeight - L1ScaleOffset / 2
+    local BaseBoxL1X = ScreenCenterpointWidth - BaseBoxL1Width / 2
+    local BaseBoxL1Y = ScreenHeight - BaseBoxL1Height
+
+    local BaseBoxL1CornerRadius = BaseBoxCornerRadius - L1ScaleOffset
+
+    draw.RoundedBoxEx(  BaseBoxCornerRadius, 
+                        BaseBoxX, 
+                        BaseBoxY, 
+                        BaseBoxWidth, 
+                        BaseBoxHeight, 
+                        Color(0, 0, 0, 230), 
+                        BaseBoxRoundTopLeft, 
+                        BaseBoxRoundTopRight,
+                        BaseBoxRoundBottomLeft,
+                        BaseBoxRoundBottomRight
+                    )
+
+    draw.RoundedBoxEx(  BaseBoxL1CornerRadius, 
+                        BaseBoxL1X, 
+                        BaseBoxL1Y, 
+                        BaseBoxL1Width, 
+                        BaseBoxL1Height, 
+                        Color(80, 80, 80, 140), 
+                        BaseBoxRoundTopLeft, 
+                        BaseBoxRoundTopRight,
+                        BaseBoxRoundBottomLeft,
+                        BaseBoxRoundBottomRight
+                )
+
 end
 
 
