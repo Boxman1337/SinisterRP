4309bffc76ecb0ab115e3a8781b5accfc78791f9
Axel <you@example.com>
2022-10-22 23:57:48 +0200

    Progress on Zarp Healthbar

--- gamemode/modules/hud/cl_hud.lua
+++ gamemode/modules/hud/cl_hud.lua
@@ -2,13 +2,17 @@
 
 function DrawHUD()
 
+    local client = LocalPlayer()
+
     local screenWidth = ScrW()
     local screenHeight = ScrH()
+
+    local screenCenter = screenWidth / 2
 
     local mainInterfaceBoxWidth = 600
     local mainInterfaceBoxHeight = 120
 
-    local mainInterfaceCenterPoint = screenWidth / 2 - mainInterfaceBoxWidth / 2
+    local mainInterfaceCenterPoint = screenCenter - mainInterfaceBoxWidth / 2
     local mainInterfaceBottomPlacement = screenHeight - mainInterfaceBoxHeight
 
     draw.RoundedBoxEx(  20,
@@ -23,12 +27,63 @@ function DrawHUD()
                         false
                     )
 
+    local maxHealth = client:GetMaxHealth()
+    local maxArmor = client:GetMaxArmor()
+
+    local currentHealth = client:Health()
+    local currentArmor = client:Armor()
+
+    local currentHealthRatio = currentHealth / maxHealth
+    local currentArmorRatio = currentArmor / maxArmor
+
+    local mainInterfaceHealthBar = {
+        { x = mainInterfaceCenterPoint + 50, y = mainInterfaceBottomPlacement + 32},
+        { x = mainInterfaceCenterPoint + 550, y = mainInterfaceBottomPlacement + 32},
+        { x = mainInterfaceCenterPoint + 575, y = mainInterfaceBottomPlacement + 60},
+        { x = mainInterfaceCenterPoint + 25 , y = mainInterfaceBottomPlacement + 60}
+    }
+
+    local mainInterfaceCurrentHealth = {
+        { x = mainInterfaceCenterPoint + 55 + (screenCenter - mainInterfaceBoxWidth / 2 + 55) * (1 - currentHealthRatio), y = mainInterfaceBottomPlacement + 35},
+        { x = mainInterfaceCenterPoint + 545, y = mainInterfaceBottomPlacement + 35},
+        { x = mainInterfaceCenterPoint + 565, y = mainInterfaceBottomPlacement + 57},
+        { x = mainInterfaceCenterPoint + 35 + (screenCenter - mainInterfaceBoxWidth / 2 + 55) * (1 - currentHealthRatio), y = mainInterfaceBottomPlacement + 57}
+    }
+
+    local mainInterfaceArmorBar = {
+        { x = mainInterfaceCenterPoint + 25, y = mainInterfaceBottomPlacement + 66},
+        { x = mainInterfaceCenterPoint + 575, y = mainInterfaceBottomPlacement + 66},
+        { x = mainInterfaceCenterPoint + 550, y = mainInterfaceBottomPlacement + 85},
+        { x = mainInterfaceCenterPoint + 50, y = mainInterfaceBottomPlacement + 85}
+    }
+
+    local mainInterfaceCurrentArmor = {
+        { x = mainInterfaceCenterPoint + 35, y = mainInterfaceBottomPlacement + 69},
+        { x = mainInterfaceCenterPoint + 565, y = mainInterfaceBottomPlacement + 69},
+        { x = mainInterfaceCenterPoint + 545, y = mainInterfaceBottomPlacement + 82},
+        { x = mainInterfaceCenterPoint + 55, y = mainInterfaceBottomPlacement + 82}
+    }
+
+    surface.SetDrawColor(120, 120, 120, 60)
+    draw.NoTexture()
+    surface.DrawPoly(mainInterfaceHealthBar)
+    surface.DrawPoly(mainInterfaceArmorBar)
+
+    surface.SetDrawColor(255, 0, 0, 200)
+    surface.DrawPoly(mainInterfaceCurrentHealth)
+
+    surface.SetDrawColor(0, 120, 200, 200)
+    surface.DrawPoly(mainInterfaceCurrentArmor)
+
+    --[[
+
     local Mat = Material("hud/Resourcebar.png", vertexlitgeneric)
 
     surface.SetMaterial(Mat)
     surface.SetDrawColor(225, 20, 20, 255)
     surface.DrawTexturedRect(mainInterfaceCenterPoint + 20, mainInterfaceBottomPlacement + 25, 560, 36)
 
+    ]]--
 end
 
 hook.Add("HUDPaint", "DrawHUD", DrawHUD)
